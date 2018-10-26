3d09267c857c7245aea91ab2379b8c1b989f28bc
ach74 <32370243+ach74@users.noreply.github.com>
2018-10-26 02:58:39 +0200

    ach

--- js/js.js
+++ js/js.js
@@ -1,7 +1,22 @@
-window.onload = function() {
+/*window.onload = function() {
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.moveTo(200,500);
 ctx.lineTo(500,0);
 ctx.stroke();
-};
+};*/
+
+window.onload = function(){
+
+	init();
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+}
+
+
+function init(){
+	var interval = window.setInterval(function(){
+		console.log("Intervalo");},1000);
+	setTimeout(function(){window.clearInterval(interval);},10000);
+}
